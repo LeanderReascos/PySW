2d7f6062c97bf6e71d6600a6013d486863d305dd
Francesco Diotallevi <francesco@pc03qcode.physik.uni-augsburg.de>
2024-06-07 13:58:00 +0200

    started working on sympy utils.py

--- Modules/sympy/classes.py
+++ Modules/sympy/classes.py
@@ -1,10 +1,9 @@
 from sympy.physics.quantum import Operator
 from sympy.core.expr import Expr
 from sympy.core.symbol import Symbol
+from sympy.core.sympify import sympify
 from sympy.printing.latex import print_latex
-from sympy import eye, kronecker_product
-
-
+from sympy import eye
 
 class RDOperator(Operator):
     @property
@@ -13,51 +12,37 @@ class RDOperator(Operator):
 
     @property
     def subspace(self):
-        if subspace == "finite":
-            raise ValueError("The subspace 'finite' is reserved for ansatz operators and should not be used for other")
         return self.args[1]
-
+    
     @property
-    def matrix(self):
-        return self.args[2]
-
-    @property
-    def is_infinite(self):
-        return self.args[3]
-    
-    
-    def subspace_identity(self):
-        return eye((self.matrix.shape)[0])
-    
     def dim(self):
-        return (self.matrix.shape)[0] if not is_infinite else 0
+        """if dim == -1 -> infinite = true"""
+        return self.args[2]
     
-    def order(self):
-        return min([coeff.order for coeff in self.matrix])
+    @property
+    def subspace_identity(self):
+        if self.dim != -1:
+            return eye(self.dim)  
+        else: 
+            return 1
     
-    def domain_expansion(self, identities, subspaces):
-        if self.is_infinite:
-            raise ValueError("The operator is infinite and cannot be expanded")
-        matrices = [identities[subspace] if subspace != self.subspace else self.matrix for subspace in subspaces]
-        new_operator = RDOperator(self.name, subspace = "S", matrix = kronecker_product(*matrices), is_infinite = False )
-        new_operator.subspace = "finite"
-        return new_operator
+    @property
+    def infinite(self):
+        return self.dim == -1
     
+    def _sympystr(self, printer):
+        return printer._print(self.name)
     
-    def __str__(self):
-        return self.name.__str__()
+    def _latex(self, printer):
+        return printer._print(self.name)
     
-    def __repr__(self):
-        return self.name.__repr__()
-     
-    def _repr_latex_(self):
-        return self.name._repr_latex_()
-    
-    def __new__(cls, name, subspace, matrix, is_infinite=False):
-        return Operator.__new__(cls, name, subspace, matrix, is_infinite)
+    def __new__(cls, name, subspace, dim):
+        if subspace == "finite":
+            raise ValueError("The subspace 'finite' is reserved for ansatz operators and should not be used for other.")
+        return Operator.__new__(cls, name, subspace, dim)
 
 
-class RDsymbols(Symbol):
+class RDsymbol(Expr):
     @property
     def name(self):
         return self.args[0]
@@ -65,49 +50,14 @@ class RDsymbols(Symbol):
     @property
     def order(self):
         return self.args[1]
-
+    
+    
+    def _sympystr(self, printer):
+        return printer._print(self.name)
+    
+    def _latex(self, printer):
+        return printer._print(self.name)
+    
+    
     def __new__(cls, name, order = 1):
-        return Symbol.__new__(cls, name, order)
-
-
-def sw_is_infinite(expr):
-    if not expr.args:
-        return False
-
-    for term in expr.args:
-        if isinstance(term, SWOperator):
-            if term.infinite:
-                return True
-        else:
-            return sw_is_infinite(term)
-
-
-def sw_is_diagonal_subspace(expr, subspace):
-    def diagonal_subspace(expr, subspace):
-        if not expr.args:
-            return 0
-
-        total = sp.Matrix()
-        for term in expr.args:
-            if isinstance(term, SWOperator):
-                if term.subspace == subspace:
-                    total += term.mat
-            else:
-                total += diagonal_subspace(term, subspace)
-        return total
-
-    total = diagonal_subspace(expr, subspace)
-    return total == 0
-
-
-def sw_get_order(expr):
-    if not expr.args:
-        return 0
-
-    total = 0
-    for term in expr.args:
-        if isinstance(term, Coefficient):
-            total += term.order
-        else:
-            total += sw_get_order(term)
-    return total
+        return Expr.__new__(cls, sympify(name), sympify(order))
---
+++ Modules/sympy/utils.py
@@ -0,0 +1,95 @@
+from Modules.sympy.classes import *
+from numpy import prod
+from sympy.core.numbers import Integer, Float, ImaginaryUnit, One, Half, Rational
+from sympy import eye
+
+numbers_list = [int, float, complex, Integer, Float, ImaginaryUnit, One, Half, Rational]
+
+def get_terms_and_factors(expr):
+    """Returns tuple of two lists: one containing ordered terms within expr, the second 
+        containing lists of factors for each term."""
+    expr = expr.expand()
+    terms = expr.as_ordered_terms()
+    factors_of_terms = [term.as_ordered_factors() for term in terms]
+    return terms, factors_of_terms
+
+def order(expr):
+    """Returns dict of expressions separated into orders.
+    """
+    
+    if isinstance(expr, RDsymbol):
+        return expr.order
+    if isinstance(expr, RDOperator) or type(expr) in numbers_list:
+        return 0
+    terms, factors_of_terms = get_terms_and_factors(expr)
+    order_of_terms = [sum([order(factor) for factor in term ]) for term in factors_of_terms]
+    order_separated = dict()
+    
+    for orderr, term in zip(order_of_terms, terms):
+        if orderr not in order_separated:
+            order_separated[orderr] = 0
+        order_separated[orderr] += term
+    return order_separated
+
+def is_infinite(expr):
+    """Returns dict of expressions separated into subspaces."""
+    if isinstance(expr, RDsymbol) or type(expr) in numbers_list:
+        return False
+    if isinstance(expr, RDOperator):
+        return expr.infinite
+    terms, factors_of_terms = get_terms_and_factors(expr)
+    infinity_of_terms = [bool(sum([is_infinite(factor) for factor in term ])) for term in factors_of_terms]
+    infinity_separated = dict()
+    
+    for infinity, term in zip(infinity_of_terms, terms):
+        if infinity not in infinity_separated:
+            infinity_separated[infinity] = 0
+        infinity_separated[infinity] += term
+    return infinity_separated
+        
+def get_finite_identities(expr):
+    """Returns dict of sympy matrices for each subspace spanned by expr."""
+    if isinstance(expr, RDsymbol) or type(expr) in numbers_list:
+        return dict()
+    if isinstance(expr, RDOperator):
+        if expr.infinite:
+            return dict()
+        return {expr.subspace : eye(expr.dim)}
+    terms, factors_of_terms = get_terms_and_factors(expr)
+    finite_subspaces = set([factor.subspace for term in factors_of_terms for factor in term if isinstance(factor, RDOperator)])
+    identities = dict()
+    for term in factors_of_terms: #"Can we avoid all these for loops?"
+        for factor in term:
+            if isinstance(factor, RDOperator):
+                if factor.subspace in identities:
+                    pass
+                identities.update(get_finite_identities(factor))
+            else:
+                get_finite_identities(factor)
+    return identities
+        
+def domain_expansion(expr, subs_dict, subspaces, identities):
+    """Returns sympy Matrix of expanded expression.
+    expr is expression to expand.
+    subs_dict is dictionary containing substitution rules for each operator into matrix form.
+    subspaces is ordered list of subspaces (order will indicate in which order to perform kronecker_product).
+    identities is list of identity operators in each subspace.
+    """
+    
+    if isinstance(expr, RDsymbol) or type(expr) in numbers_list:
+        return expr
+    if isinstance(expr, RDOperator):
+        if expr.infinite:
+            return expr
+        print(identities)
+        print(expr)
+        matrices = [identities[subspace] if subspace != expr.subspace else expr.subs(subs_dict) for subspace in subspaces]
+        return kronecker_product(*matrices)
+    
+    terms, factors_of_terms = get_terms_and_factors(expr)
+    
+    matrices = []
+    for term in factors_of_terms: # "Can we avoid all these for loops?"
+        for factor in term:
+            matrices.append(domain_expansion(factor))
+    return prod(matrices)
